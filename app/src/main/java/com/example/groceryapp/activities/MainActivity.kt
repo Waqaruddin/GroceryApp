b04f362a9d1170518441b90b81185262ccb321af
Syed Waqar Uddin <49626225+Waqaruddin@users.noreply.github.com>
2020-09-24 21:00:04 -0400

    Drawer navigation added

--- app/src/main/java/com/example/groceryapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/groceryapp/activities/MainActivity.kt
@@ -1,33 +1,37 @@
 package com.example.groceryapp.activities
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.groceryapp.R
 import com.example.groceryapp.adapters.AdapterCategory
-import com.example.groceryapp.app.Config
 import com.example.groceryapp.app.Endpoints
 import com.example.groceryapp.helpers.SessionManager
 import com.example.groceryapp.models.Category
 import com.example.groceryapp.models.CategoryResult
+import com.google.android.material.navigation.NavigationView
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar.*
-import org.json.JSONArray
 
-class MainActivity : AppCompatActivity() {
-    lateinit var sessionManager:SessionManager
-    var mList:ArrayList<Category> = ArrayList()
-    lateinit var adapterCategory:AdapterCategory
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    lateinit var sessionManager: SessionManager
+    lateinit var drawerLayout:DrawerLayout
+    var mList: ArrayList<Category> = ArrayList()
+    lateinit var navView:NavigationView
+    lateinit var adapterCategory: AdapterCategory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -36,7 +40,7 @@ class MainActivity : AppCompatActivity() {
         init()
     }
 
-    private fun setupToolbar(){
+    private fun setupToolbar() {
         var toolbar = tool_bar
         toolbar.title = "Grocery App"
         setSupportActionBar(toolbar)
@@ -45,9 +49,25 @@ class MainActivity : AppCompatActivity() {
     private fun init() {
         setupToolbar()
         getData()
+        navView = nav_view
+        drawerLayout = drawer_layout
+        navView.setNavigationItemSelectedListener(this)
+
+        var toggle = ActionBarDrawerToggle( this, drawerLayout, tool_bar, 0,0)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
         adapterCategory = AdapterCategory(this)
         recycler_view.layoutManager = GridLayoutManager(this, 2)
         recycler_view.adapter = adapterCategory
+    }
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
+
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -56,17 +76,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-         when (item.itemId){
+        when (item.itemId) {
             R.id.action_cart -> startActivity(Intent(this, CartActivity::class.java))
-             R.id.action_profile -> Toast.makeText(applicationContext, "Profile", Toast.LENGTH_SHORT).show()
-             R.id.action_setting -> Toast.makeText(applicationContext, "Setting", Toast.LENGTH_SHORT).show()
-             R.id.action_logout -> {sessionManager.logout()
-                 startActivity(Intent(applicationContext, LoginActivity::class.java))}
+            R.id.action_profile -> Toast.makeText(applicationContext, "Profile", Toast.LENGTH_SHORT)
+                .show()
+            R.id.action_setting -> Toast.makeText(applicationContext, "Setting", Toast.LENGTH_SHORT)
+                .show()
+            R.id.action_logout -> {
+                sessionManager.logout()
+                startActivity(Intent(applicationContext, LoginActivity::class.java))
+            }
 
 
-         }
+        }
         return true
     }
+
 
     private fun getData() {
         var request = StringRequest(
@@ -75,10 +100,10 @@ class MainActivity : AppCompatActivity() {
             {
                 var gson = Gson()
                 var categoryResult = gson.fromJson(it, CategoryResult::class.java)
-               // Log.d("abc", categoryResult.data[0].catName)
+                // Log.d("abc", categoryResult.data[0].catName)
                 mList.addAll(categoryResult.data)
                 adapterCategory.setData(mList)
-                progress_bar.visibility = View.GONE
+                // progress_bar.visibility = View.GONE
 
             },
             {
@@ -89,4 +114,39 @@ class MainActivity : AppCompatActivity() {
         )
         Volley.newRequestQueue(this).add(request)
     }
+
+    private fun dialogueLogout() {
+        var builder = AlertDialog.Builder(this)
+        builder.setTitle("Logout")
+        builder.setMessage("Are you sure you want to logout?")
+        builder.setPositiveButton("Yes", object:DialogInterface.OnClickListener {
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                sessionManager.logout()
+            }
+
+        })
+        builder.setNegativeButton("No", object:DialogInterface.OnClickListener {
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                dialog?.dismiss()
+            }
+
+        })
+        var alertDialog = builder.create()
+        alertDialog.show()
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_logout -> dialogueLogout()
+            R.id.item_account -> Toast.makeText(this, "account", Toast.LENGTH_SHORT).show()
+            R.id.item_settings -> Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show()
+            R.id.item_orders -> Toast.makeText(this, "orders", Toast.LENGTH_SHORT).show()
+
+        }
+        return true
+    }
+
+
+
 }
