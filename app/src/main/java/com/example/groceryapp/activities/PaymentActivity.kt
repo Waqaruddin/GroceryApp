4dcc9053642ef800e99e892b58583d7b0da9c2c9
Syed Waqar Uddin <49626225+Waqaruddin@users.noreply.github.com>
2020-09-24 23:38:35 -0400

    Fix Payment UI

--- app/src/main/java/com/example/groceryapp/activities/PaymentActivity.kt
+++ app/src/main/java/com/example/groceryapp/activities/PaymentActivity.kt
@@ -2,6 +2,7 @@ package com.example.groceryapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.groceryapp.R
 import com.example.groceryapp.database.DBHelper
 import com.example.groceryapp.models.Product
@@ -23,8 +24,18 @@ class PaymentActivity : AppCompatActivity() {
     private fun init() {
         mList = dbHelper.getProduct()
         var totals = dbHelper.getTotal(mList)
-        text_view_subtotal.text = totals.subtotal.toString()
-        text_view_discount.text = totals.discount.toString()
-        text_view_order_amount.text = totals.total.toString()
+        text_view_subtotal.text = "Subtotal " + totals.subtotal.toString()
+        text_view_discount.text = "Discount " + totals.discount.toString()
+        text_view_order_amount.text = "Total " + totals.total.toString()
+        text_view_total_amount.text = totals.total.toString()
+
+        button_pay_online.setOnClickListener {
+            Toast.makeText(this, "Accepting Cash Only", Toast.LENGTH_SHORT).show()
+        }
+
+        button_pay_delivery.setOnClickListener {
+            Toast.makeText(this, "Cash on Delivery", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
